49f85dbfa6291dafbda235fe30312ab19578fc13
Rico Schiekel <fire@downgra.de>
2010-09-11 01:24:36 +0200

    add diff lexer implementation with unit tests

--- src/main/scala/de/downgra/slayon/lexer/DiffLexer.scala
+++ src/main/scala/de/downgra/slayon/lexer/DiffLexer.scala
@@ -1,13 +1,34 @@
 package de.downgra.slayon.lexer
 
-import scala.util.parsing.combinator.RegexParsers
+import util.parsing.combinator.RegexParsers
+import util.matching.Regex
+import util.parsing.combinator.{Parsers, RegexParsers}
 
-import de.downgra.slayon.Token
+import de.downgra.slayon.token.Token
+import de.downgra.slayon.token.Text
+import de.downgra.slayon.token.Generic.{Inserted, Deleted, Subheading, Heading}
 
 object DiffLexer extends RegexParsers {
+  override val skipWhitespace = false
 
-  def parse(value: String): List[Token] = List()
+  private def inserted = """(?m)^\+.*\n*""".r ^^ {
+    Inserted(_)
+  }
+  private def deleted = """(?m)^-.*\n*""".r ^^ {
+    Deleted(_)
+  }
+  private def subheading = """(?m)^@.*\n*""".r ^^ {
+    Subheading(_)
+  }
+  private def heading = ("""(?m)^Index.*\n*""".r | """(?m)^=.*\n*""".r) ^^ {
+    Heading(_)
+  }
+  private def text = """(?m)^.*\n*""".r ^^ {
+    Text(_)
+  }
+  private def diff = (subheading | heading | inserted | deleted | text) *
 
+  def parse(value: String): ParseResult[List[Token]] = parseAll(diff, value)
 }
 
 // vim: set ts=2 sw=2 et:
--- src/test/scala/DiffSpec.scala
+++ src/test/scala/DiffSpec.scala
@@ -4,11 +4,65 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
 import lexer.DiffLexer
+import token.Token
+import token.Text
+import token.Generic.{Inserted, Deleted, Subheading, Heading}
 
 class DiffSpec extends FlatSpec with ShouldMatchers {
 
-  "An empty input" should "result in an empty list of tokens" in {
-    DiffLexer.parse("").length should be === 0    
+  "Empty input" should "result in an empty list of tokens" in {
+    val res = DiffLexer.parse("")
+    res.successful should be === true
+    res.get.length should be === 0
+  }
+
+
+  "A single normal line" should "produce a single text token" in {
+    testPositiv(Text("abcde"))
+    testPositiv(Text("test 123"))
+    testPositiv(Text(" foo bar"))
+  }
+
+  "A single line with an insertion" should "produce a single insertion token" in {
+    testPositiv(Inserted("+abcde"))
+    testPositiv(Inserted("+++test"))
+  }
+
+  "A single line with an deletion" should "produce a single deletion token" in {
+    testPositiv(Deleted("-ab cd e"))
+    testPositiv(Deleted("---test"))
+  }
+
+  "A single line with an heading" should "produce a single heading token" in {
+    testPositiv(Heading("Index: foo"))
+    testPositiv(Heading("=foo"))
+  }
+
+  "A single line with an subheading" should "produce a single subheading token" in {
+    testPositiv(Subheading("@bar"))
+    testPositiv(Subheading("@spam eggs"))
+  }
+
+  "A simple input with all possible types" should "produce the correct resultlist" in {
+    testPositiv("-ab\n+cd\n=foo\ntesttext line\nIndex:bar\n@spam\n",
+                List(Deleted("-ab\n"),
+                     Inserted("+cd\n"),
+                     Heading("=foo\n"),
+                     Text("testtext line\n"),
+                     Heading("Index:bar\n"),
+                     Subheading("@spam\n")))
+  }
+
+  private def testPositiv(value: Token) {
+    testPositiv(value.content, List(value))
+  }
+
+  private def testPositiv(value: String, result: List[Token]) {
+    val res = DiffLexer.parse(value)
+    res.successful should be === true
+    res.get.length should be === result.length
+    res.get should be === result
+    res.get map (_.content) mkString("") should be === value
   }
 
 }

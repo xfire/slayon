4c33b9f7e0d1b1bd2023ba3325e6cbf7e0ef145d
Rico Schiekel <fire@downgra.de>
2010-09-10 20:29:27 +0200

    add project configuration file

---
+++ project/build/SlayonProject.scala
@@ -0,0 +1,11 @@
+import sbt._
+
+class SlayonProject(info: ProjectInfo) extends DefaultProject(info) {
+  // External dependencies
+
+  val scalaToolsSnapshots = ScalaToolsSnapshots
+
+  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+}
+
+// vim: set ts=2 sw=2 et:

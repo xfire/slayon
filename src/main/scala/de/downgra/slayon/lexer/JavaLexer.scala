efc50ff50566cb2f6f1ae6667fe53be7dbef7d00
Rico Schiekel <fire@downgra.de>
2010-09-17 21:19:48 +0200

    finish java lexer

--- src/main/scala/de/downgra/slayon/lexer/JavaLexer.scala
+++ src/main/scala/de/downgra/slayon/lexer/JavaLexer.scala
@@ -4,8 +4,9 @@ import util.parsing.combinator.RegexParsers
 import util.matching.Regex
 import util.parsing.combinator.{Parsers, RegexParsers}
 
-import de.downgra.slayon.token.{Token, Text, Whitespace, Keyword, Operator}
-import de.downgra.slayon.token.{Comments, Names, Keywords}
+import de.downgra.slayon.token.{Token, Whitespace, Keyword, Operator,
+                                String => LString, Name}
+import de.downgra.slayon.token.{Comments, Names, Keywords, Strings, Numbers}
 
 object JavaLexer extends RegexLexer {
   val name = "Java"
@@ -15,39 +16,69 @@ object JavaLexer extends RegexLexer {
 
   override val defaultRegexFlags = "(?m)"
 
-  private def ws = """\s*""".re
+  private def ws = """\s+""".re
+  private def wso = """\s*""".re
 
-  private def lineBreak = """[\n\r]+""".re
-  private def commentSingle = """//.*""".re
-  private def commentMulti = """/\*[^\*]*\*/""".re // no nestes comments
-  private def decorator = """@[\p{Alpha}_][\p{Alnum}_\.]*""".re
-  private def keyword = ("""(assert|break|case|catch|continue|default|do|""" +
+  private def jlineBreak = """[\n\r]+""".re
+  private def jcommentSingle = """//.*""".re
+  private def jcommentMulti = """/\*[^\*]*\*/""".re // no nestes comments
+  private def jdecorator = """@[\p{Alpha}_][\p{Alnum}_\.]*""".re
+  private def jkeyword = ("""(assert|break|case|catch|continue|default|do|""" +
                          """else|finally|for|if|goto|instanceof|new|return|""" +
                          """switch|this|throw|try|while)\b""").re
-  private def declKeyword = ("""(abstract|const|enum|extends|final|implements|""" +
+  private def jdeclKeyword = ("""(abstract|const|enum|extends|final|implements|""" +
                              """native|private|protected|public|static|strictfp|""" +
                              """super|synchronized|throws|transient|volatile)\b""").re
-  private def typeKeyword = """(boolean|byte|char|double|float|int|long|short|void)\b""".re
+  private def jtypeKeyword = """(boolean|byte|char|double|float|int|long|short|void)\b""".re
+  private def jpackage = "package" ~ ws
+  private def jconstant = """(true|false|null)\b""".re
+  private def jclassInterface = """class|interface""".re ~ ws ~ """[a-zA-Z_][a-zA-Z0-9_]+""".re
+  private def jimport = "import" ~ ws ~ """[a-zA-Z0-9_.]+\*?""".re
+  // the vim scala syntax highlighter don't like the following line ;)
+  private def jstring = """\"(\\\\|\\"|[^"])*\"""".re
+  private def jchar = """'(\\.|[^\\]|\\u[0-9a-fA-F]{4}|\\[0-3][0-7]{0,2})'""".re
+  private def jattribute = """(\.)""".re ~ """([a-zA-Z_][a-zA-Z0-9_]*)""".re
+  private def jlabel = """[a-zA-Z_][a-zA-Z0-9_]*:""".re
+  private def jname = """[a-zA-Z_\$][a-zA-Z0-9_]*""".re
+  private def joperator = """[~\^\*!%&\[\]\(\)\{\}<>\|+=:;,./?-]""".re
 
+  private def jfloat = """(([1-9][0-9]+\.[0-9]*)|([1-9]*\.[0-9]+)|([1-9]+(?=[eE])))([eE][-+]?[0-9]+)?[fd]?""".re
+  private def jhex = """0x[0-9a-fA-F]+""".re
+  private def jinteger = """[0-9]+[lL]?""".re
 
+  // method
   private def methodModifier = ("""(public|protected|private|static|strictfp|native|""" +
                                 """abstract|final|synchronized)\b""").re
-  private def methodReturnType = """[a-zA-Z_][a-zA-Z0-9_\.\[\]]*""".re
-  private def methodName = """[a-zA-Z_][a-zA-Z0-9_]*""".re
-  private def methodDefinition = ((methodModifier ~ """\s+""".re)*) ~ ws ~ methodReturnType ~ ws ~
-                                 methodName ~ ws ~ """\(""".re
-
+  private def methodReturnType = """[a-zA-Z_][a-zA-Z0-9_\.\[\]]+""".re
+  private def methodName = """[a-zA-Z_][a-zA-Z0-9_]+""".re
+  private def jmethodDefinition = ((methodModifier ~ """\s+""".re)*) ~ wso ~
+                                 methodReturnType ~ wso ~
+                                 methodName ~ wso ~ """\(""".re
 
   private def java = 
-    ( commentSingle %% Comments.Single
-    | commentMulti  %% Comments.Multiline
-    | decorator %% Names.Decorator
-    | methodDefinition *% (List(Keywords.Declaration, Text),
-                           List(Text, Keywords.Type, Text, Names.Function, Text, Operator))
-    | keyword %% Keyword
-    | declKeyword %% Keywords.Declaration
-    | typeKeyword %% Keywords.Type
-    | lineBreak %% Whitespace
+    ( jcommentSingle %% Comments.Single
+    | jcommentMulti  %% Comments.Multiline
+    | jdecorator %% Names.Decorator
+    | jmethodDefinition *% (List(Keywords.Declaration, Whitespace),
+                            List(Whitespace, Keywords.Type, Whitespace, Names.Function, Whitespace, Operator))
+    | jkeyword %% Keyword
+    | jdeclKeyword %% Keywords.Declaration
+    | jtypeKeyword %% Keywords.Type
+    | jconstant %% Keywords.Constant
+    | jpackage %%% (Keywords.Namespace, Whitespace)
+    | jclassInterface %%% (Keywords.Declaration, Whitespace, Names.Class)
+    | jimport %%% (Keywords.Namespace, Whitespace, Names.Namespace)
+    | jstring %% LString
+    | jchar %% Strings.Char
+    | jattribute %%% (Operator, Names.Attribute)
+    | jlabel %% Names.Label
+    | jname %% Name
+    | joperator %% Operator
+    | jhex %% Numbers.Hex
+    | jfloat %% Numbers.Float
+    | jinteger %% Numbers.Integer
+    | jlineBreak %% Whitespace
+    | ws %% Whitespace
     ) *
 
   def parse(value: String): Result = makeResult(parseAll(java, value))
--- src/test/scala/JavaSpec.scala
+++ src/test/scala/JavaSpec.scala
@@ -4,8 +4,8 @@ import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
 import lexer.JavaLexer
-import token.{Token, Text, Whitespace, Keyword, Operator}
-import token.{Comments, Names, Keywords}
+import token.{Token, Whitespace, Keyword, Operator, String => LString, Name}
+import token.{Comments, Names, Keywords, Strings, Numbers}
 
 class JavaSpec extends FunSuite with ShouldMatchers with TokenTesters {
 
@@ -64,17 +64,128 @@ class JavaSpec extends FunSuite with ShouldMatchers with TokenTesters {
   }
 
   test("single method definition") {
-    testPositiv("void foo (", List(Keywords.Type("void"), Text(" "),
-                                   Names.Function("foo"), Text(" "),
+    testPositiv("void foo (", List(Keywords.Type("void"), Whitespace(" "),
+                                   Names.Function("foo"), Whitespace(" "),
                                    Operator("(")))
-    testPositiv("public Person getPerson(", List(Keywords.Declaration("public"), Text(" "),
-                                                 Keywords.Type("Person"), Text(" "),
+    testPositiv("public Person getPerson(", List(Keywords.Declaration("public"), Whitespace(" "),
+                                                 Keywords.Type("Person"), Whitespace(" "),
                                                  Names.Function("getPerson"), Operator("(")))
 
-    testPositiv("public static int getX(", List(Keywords.Declaration("public"), Text(" "),
-                                                Keywords.Declaration("static"), Text(" "),
-                                                Keywords.Type("int"), Text(" "),
+    testPositiv("public static int getX(", List(Keywords.Declaration("public"), Whitespace(" "),
+                                                Keywords.Declaration("static"), Whitespace(" "),
+                                                Keywords.Type("int"), Whitespace(" "),
                                                 Names.Function("getX"), Operator("(")))
+  }
+
+  test("single package definition") {
+    testPositiv("package ", List(Keywords.Namespace("package"), Whitespace(" ")))
+    testPositiv("package   ", List(Keywords.Namespace("package"), Whitespace("   ")))
+  }
+
+  test("single constants") {
+    "true|false|null".split('|').foreach(s => testPositiv(Keywords.Constant(s)))
+  }
+
+  test("single class/interface definition") {
+    for(what <- List("class", "interface")) {
+      testPositiv(what + " FooBar", List(Keywords.Declaration(what), Whitespace(" "), Names.Class("FooBar")))
+      testPositiv(what + "  FooBar", List(Keywords.Declaration(what), Whitespace("  "), Names.Class("FooBar")))
+      testPositiv(what + "\nFooBar", List(Keywords.Declaration(what), Whitespace("\n"), Names.Class("FooBar")))
+    }
+  }
+
+  test("single import statement") {
+    testPositiv("import foo", List(Keywords.Namespace("import"), Whitespace(" "), Names.Namespace("foo")))
+    testPositiv("import\nfoo.bar", List(Keywords.Namespace("import"),
+                                        Whitespace("\n"),
+                                        Names.Namespace("foo.bar")))
+    testPositiv("import  foo.bar.*", List(Keywords.Namespace("import"),
+                                          Whitespace("  "),
+                                          Names.Namespace("foo.bar.*")))
+  }
+
+  test("single string") {
+    testPositiv(LString("\"foo\""))
+    testPositiv(LString("\"foo bar\""))
+    testPositiv(LString("\"foo\\nbar\""))
+    testPositiv(LString("\"foo \\t  bar\\\"spam\\\"\""))
+  }
+
+  test("single char") {
+    testPositiv(Strings.Char("'\\n'"))
+    testPositiv(Strings.Char("'.'"))
+    testPositiv(Strings.Char("' '"))
+    testPositiv(Strings.Char("'a'"))
+    testPositiv(Strings.Char("'Z'"))
+    testPositiv(Strings.Char("'9'"))
+    testPositiv(Strings.Char("'\\0'"))
+    testPositiv(Strings.Char("'\\30'"))
+    testPositiv(Strings.Char("'\\377'"))
+    testPositiv(Strings.Char("'\\u0000'"))
+    testPositiv(Strings.Char("'\\uffff'"))
+  }
+
+  test("single attribute") {
+    testPositiv("._foo", List(Operator("."), Names.Attribute("_foo")))
+    testPositiv(".foo", List(Operator("."), Names.Attribute("foo")))
+    testPositiv("._foo23", List(Operator("."), Names.Attribute("_foo23")))
+    testPositiv(".foo23", List(Operator("."), Names.Attribute("foo23")))
+    testPositiv("._foo_23", List(Operator("."), Names.Attribute("_foo_23")))
+    testPositiv(".foo_23", List(Operator("."), Names.Attribute("foo_23")))
+  }
+
+  test("single label") {
+    testPositiv(Names.Label("foo:"))
+    testPositiv(Names.Label("_f_oo42:"))
+  }
+
+  test("single name") {
+    testPositiv(Name("foo"))
+    testPositiv(Name("_foo_"))
+    testPositiv(Name("$foo_"))
+  }
+
+  test("single operator") {
+    "~^*!%&[](){}<>|+=:;,.?-".foreach(s => testPositiv(Operator(s.toString)))
+  }
+
+  test("single float") {
+    testPositiv(Numbers.Float("1.41421"))
+    testPositiv(Numbers.Float("2e6"))
+    testPositiv(Numbers.Float("3.77f"))
+    testPositiv(Numbers.Float("3.7E-4f"))
+  }
+
+  test("single hex") {
+   testPositiv(Numbers.Hex("0xCAFE"))
+   testPositiv(Numbers.Hex("0xCaFeBaBe"))
+  }
+
+  test("single integer") {
+   testPositiv(Numbers.Integer("0"))
+   testPositiv(Numbers.Integer("23l"))
+   testPositiv(Numbers.Integer("42L"))
+  }
+
+  test("simple example") {
+    testPositiv("""|public class HalloWelt {
+                   |  public static void main(String[] args) {
+                   |    System.out.println("Hallo Welt!");
+                   |  }
+                   |}
+                   |""".stripMargin,
+                List(Keywords.Declaration("public"), Whitespace(" "), Keywords.Declaration("class"),
+                     Whitespace(" "), Names.Class("HalloWelt"), Whitespace(" "), Operator("{"),
+                     Whitespace("\n"), Whitespace("  "), Keywords.Declaration("public"),
+                     Whitespace(" "), Keywords.Declaration("static"), Whitespace(" "),
+                     Keywords.Type("void"), Whitespace(" "), Names.Function("main"), Operator("("),
+                     Name("String"), Operator("["), Operator("]"), Whitespace(" "), Name("args"),
+                     Operator(")"), Whitespace(" "), Operator("{"), Whitespace("\n"),
+                     Whitespace("    "), Name("System"), Operator("."), Names.Attribute("out"),
+                     Operator("."), Names.Attribute("println"), Operator("("), LString("\"Hallo Welt!\""),
+                     Operator(")"), Operator(";"), Whitespace("\n"), Whitespace("  "), Operator("}"),
+                     Whitespace("\n"), Operator("}"), Whitespace("\n")))
+
   }
 }
 

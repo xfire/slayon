6164dfc2f7916ee2e3c18932e9f7e983f7b3b365
Rico Schiekel <fire@downgra.de>
2010-09-18 17:56:57 +0200

    add formatters package

---
+++ src/main/scala/de/downgra/slayon/formatters/package.scala
@@ -0,0 +1,14 @@
+package de.downgra.slayon
+
+import java.io.Writer
+import token.Token
+
+package object formatters {
+
+  trait Formatter {
+    def format(tokens: Seq[Token], writer: Writer)
+  }
+
+}
+
+// vim: set ts=2 sw=2 et:
